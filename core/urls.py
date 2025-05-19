c79a7f5cf7d23e0ad4255fb9126f328698a94f25
tim <simwawatimothy64@gmail.com>
2025-05-19 21:07:36 +0200

    working

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import home, login_user, logout_user, traffic, ssh_connection, start_flask_app, stop_flask_app, check_flask_app_status, recent_connections
+from core import views
 
 app_name = "core"
 urlpatterns = [
@@ -7,6 +8,7 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('traffic/', traffic, name='traffic'),
+    path('authenticant-user/', views.login_user, name='login_user'),
     path('ssh-connection/', ssh_connection, name='ssh_connection'),
     path('start-flask-app/', start_flask_app, name='start_flask_app'),
     path('stop-flask-app/', stop_flask_app, name='stop_flask_app'),
